4fbb65a0291d6ae0968e1800828d35db3386451b
baojun <317856353@qq.com>
2018-09-24 21:37:39 +0800

    init

---
+++ src/main/java/com/skm/mch/wx/mp/Application.java
@@ -0,0 +1,27 @@
+package com.skm.mch.wx.mp;
+
+import com.skm.common.mybatis.config.MybatisConfiguration;
+import com.skm.common.spring.security.SecurityConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@Import({SecurityConfiguration.class, MybatisConfiguration.class, Swagger2DocumentationConfiguration.class})
+@ComponentScan(basePackages = {"com.skm.mch.wx.mp", "com.skm.common.spring",  "com.skm.common.weixin", "com.skm.common.user"})
+public class Application {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext env = SpringApplication.run(Application.class, args);
+        String[] activeProfiles = env.getEnvironment().getActiveProfiles();
+        LOG.info("active profile: {}", Arrays.toString(activeProfiles));
+    }
+}
---
+++ src/main/java/com/skm/mch/wx/mp/package-info.java
@@ -0,0 +1,1 @@
+package com.skm.mch.wx.mp;
